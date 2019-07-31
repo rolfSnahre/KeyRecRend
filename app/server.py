9dd0930fe354fa8c153c5516529068d877148295
Rolf <rolf.snahre@hotmail.com>
2019-07-31 21:57:46 +0200

    Changed model to key

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1locE2LFXB_yOSyx4Oba4fVTP86k0nJEO'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Key,NoKey']
 path = Path(__file__).parent
 
 app = Starlette()
